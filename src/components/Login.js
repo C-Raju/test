002393afe1ea336cea7fb8aa0096303091d479ed
C-Raju <126947587+C-Raju@users.noreply.github.com>
2023-11-10 12:06:36 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch("https://mern-zeta-rouge.vercel.app/user/login", {
+      const response = await fetch("https://mern-1xrisztsn-c-raju.vercel.app/user/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
