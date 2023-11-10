2f21a747873e07a88b21c66198051164e3a44095
C-Raju <126947587+C-Raju@users.noreply.github.com>
2023-11-10 12:07:05 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,7 +7,7 @@ const Signup = () => {
 
   const handleSignUp = async () => {
     try {
-      const response = await fetch("https://mern-zeta-rouge.vercel.app/user/signup", {
+      const response = await fetch("https://mern-1xrisztsn-c-raju.vercel.app/user/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
